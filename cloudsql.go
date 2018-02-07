944818a3411308bbc75f7fa5d94577f6415f0516
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2018-02-06 22:37:35 -0800

    Added endpoint for searching for addresses

--- cloudsql.go
+++ cloudsql.go
@@ -10,7 +10,6 @@ import (
 	"reflect"
 
 	"google.golang.org/appengine"
-
 	_ "github.com/broady/gae-postgres"
 )
 
@@ -18,12 +17,11 @@ var db *sql.DB
 
 func main() {
 	datastoreName := os.Getenv("POSTGRES_CONNECTION")
-
 	fmt.Println(datastoreName)
 
 	var err error
 	db, err = sql.Open("gae-postgres", datastoreName)
-	//	db, err = sql.Open("postgres", datastoreName)
+	//db, err = sql.Open("postgres", datastoreName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,8 +29,10 @@ func main() {
 	http.HandleFunc("/", handle)
 	http.HandleFunc("/census-tract", censusTracts)
 	http.HandleFunc("/property-tax", propertyTax)
+	http.HandleFunc("/address-search", addressSearch)
 
 	appengine.Main()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func checkErr(err error) {
@@ -132,80 +132,11 @@ func propertyTax(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
-// NullInt64 is an alias for sql.NullInt64 data type
-type NullInt64 sql.NullInt64
-
-// Scan implements the Scanner interface for NullInt64
-func (ni *NullInt64) Scan(value interface{}) error {
-	var i sql.NullInt64
-	if err := i.Scan(value); err != nil {
-		return err
-	}
-
-	// if nil then make Valid false
-	if reflect.TypeOf(value) == nil {
-		*ni = NullInt64{i.Int64, false}
-	} else {
-		*ni = NullInt64{i.Int64, true}
-	}
-	return nil
-}
-
-// NullString is an alias for sql.NullString data type
-type NullString sql.NullString
-
-// Scan implements the Scanner interface for NullString
-func (ns *NullString) Scan(value interface{}) error {
-	var s sql.NullString
-	if err := s.Scan(value); err != nil {
-		return err
-	}
-
-	// if nil then make Valid false
-	if reflect.TypeOf(value) == nil {
-		*ns = NullString{s.String, false}
-	} else {
-		*ns = NullString{s.String, true}
-	}
-
-	return nil
-}
-
-// MarshalJSON for NullInt64
-func (ni *NullInt64) MarshalJSON() ([]byte, error) {
-	if !ni.Valid {
-		return []byte("null"), nil
-	}
-	return json.Marshal(ni.Int64)
-}
-
-// UnmarshalJSON for NullInt64
-func (ni *NullInt64) UnmarshalJSON(b []byte) error {
-	err := json.Unmarshal(b, &ni.Int64)
-	ni.Valid = (err == nil)
-	return err
-}
-
-// MarshalJSON for NullString
-func (ns *NullString) MarshalJSON() ([]byte, error) {
-	if !ns.Valid {
-		return []byte("null"), nil
-	}
-	return json.Marshal(ns.String)
-}
-
-// UnmarshalJSON for NullString
-func (ns *NullString) UnmarshalJSON(b []byte) error {
-	err := json.Unmarshal(b, &ns.String)
-	ns.Valid = (err == nil)
-	return err
-}
-
 type censusTract struct {
 	CTUID           string     `json:"ctuid"`
 	NumberOfRentals int        `json:"number_of_rentals"`
 	AveragePrice    int        `json:"average_price"`
-	MedianPrice		int 		`json:"median_price"`
+	MedianPrice     int        `json:"median_price"`
 	MinPrice        NullInt64  `json:"min_price"`
 	MaxPrice        int        `json:"max_price"`
 	AverageSqFt     NullInt64  `json:"average_sqft"`
@@ -239,7 +170,6 @@ func censusTracts(w http.ResponseWriter, r *http.Request) {
 			AND r.bedrooms IN (%v)
 		) as ctr GROUP BY ctuid`, bedrooms)
 
-
 	rows, err := db.Query(q, queries.Get("swlng"), queries.Get("swlat"), queries.Get("nelng"), queries.Get("nelat"))
 	checkErr(err)
 
@@ -260,4 +190,123 @@ func censusTracts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
 	w.Write(data)
+}
+
+type propertyShort struct {
+	LAND_COORDINATE			int
+	TO_CIVIC_NUMBER			int
+	FROM_CIVIC_NUMBER 		int
+	STREET_NAME 			string
+	PROPERTY_POSTAL_CODE 	string
+}
+
+func addressSearch(w http.ResponseWriter, r *http.Request) {
+	queries := r.URL.Query()
+	address := queries.Get("address")
+	lat := queries.Get("lat")
+	long := queries.Get("long")
+
+	q := `
+		SELECT
+		    pt.LAND_COORDINATE,
+			pt.TO_CIVIC_NUMBER,
+			pt.FROM_CIVIC_NUMBER,
+			pt.STREET_NAME,
+			pt.PROPERTY_POSTAL_CODE
+		FROM tbl_property_parcels AS pp
+		JOIN tbl_property_taxes AS pt
+		  ON pp.tax_coord = pt.land_coordinate
+		WHERE ST_Contains(pp.wkb_geometry, ST_SetSRID(ST_MakePoint($1, $2), 4326))
+		AND pt.to_civic_number = $3
+	`
+
+	rows, err := db.Query(q, long, lat, address)
+	checkErr(err)
+
+	defer rows.Close()
+
+	propertiesShort := make([]propertyShort, 0)
+
+	for rows.Next() {
+		ps := propertyShort{}
+		err = rows.Scan(
+			&ps.LAND_COORDINATE,
+			&ps.TO_CIVIC_NUMBER,
+			&ps.FROM_CIVIC_NUMBER,
+			&ps.STREET_NAME,
+			&ps.PROPERTY_POSTAL_CODE )
+		checkErr(err)
+
+		propertiesShort = append(propertiesShort, ps)
+	}
+
+	data, _ := json.Marshal(propertiesShort)
+
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(data)
+}
+
+
+
+// NullInt64 is an alias for sql.NullInt64 data type
+type NullInt64 sql.NullInt64
+// Scan implements the Scanner interface for NullInt64
+func (ni *NullInt64) Scan(value interface{}) error {
+	var i sql.NullInt64
+	if err := i.Scan(value); err != nil {
+		return err
+	}
+
+	// if nil then make Valid false
+	if reflect.TypeOf(value) == nil {
+		*ni = NullInt64{i.Int64, false}
+	} else {
+		*ni = NullInt64{i.Int64, true}
+	}
+	return nil
+}
+// NullString is an alias for sql.NullString data type
+type NullString sql.NullString
+// Scan implements the Scanner interface for NullString
+func (ns *NullString) Scan(value interface{}) error {
+	var s sql.NullString
+	if err := s.Scan(value); err != nil {
+		return err
+	}
+
+	// if nil then make Valid false
+	if reflect.TypeOf(value) == nil {
+		*ns = NullString{s.String, false}
+	} else {
+		*ns = NullString{s.String, true}
+	}
+
+	return nil
+}
+// MarshalJSON for NullInt64
+func (ni *NullInt64) MarshalJSON() ([]byte, error) {
+	if !ni.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(ni.Int64)
+}
+// UnmarshalJSON for NullInt64
+func (ni *NullInt64) UnmarshalJSON(b []byte) error {
+	err := json.Unmarshal(b, &ni.Int64)
+	ni.Valid = (err == nil)
+	return err
+}
+// MarshalJSON for NullString
+func (ns *NullString) MarshalJSON() ([]byte, error) {
+	if !ns.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(ns.String)
+}
+// UnmarshalJSON for NullString
+func (ns *NullString) UnmarshalJSON(b []byte) error {
+	err := json.Unmarshal(b, &ns.String)
+	ns.Valid = (err == nil)
+	return err
 }
